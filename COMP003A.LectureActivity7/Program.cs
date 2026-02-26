62d00ff90ce1904580edf8d27f81b6a6d11d88e0
ventusaguirre <ventusaguirre@my.whccd.edu>
2026-02-25 18:28:38 -0800

    added scores array, for loop to display scores; added names list, added three names to this list, added foreach and for loops to display names

--- COMP003A.LectureActivity7/Program.cs
+++ COMP003A.LectureActivity7/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace COMP003A.LectureActivity7
+﻿using System.Collections.Generic;
+
+namespace COMP003A.LectureActivity7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] scores = { 7, 9, 13, 14, 26};
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine(scores[i]);
+            }
+
+            List<string> names = new List<string>();
+
+            names.Add("Link");
+            names.Add("Zelda");
+            names.Add("Ganon");
+
+            foreach (string i in names)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]); 
+            }
+
         }
     }
 }
